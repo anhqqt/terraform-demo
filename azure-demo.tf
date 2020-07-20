ef30b66e2eba52d9d0fb3f1044743eb9433fcbb9
Anh Quach <matdecha@MacBook-Pro.local>
2020-07-21 01:03:30 +0700

    Updated terraform-demo project

--- azure-demo.tf
+++ azure-demo.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "devops" {
   location = "Southeast Asia"
 }
 
-module "jenkins-vm" {
+module "jenkins_vm" {
   source                        = "Azure/compute/azurerm"
   resource_group_name           = azurerm_resource_group.devops.name
   location                      = azurerm_resource_group.devops.location
@@ -24,7 +24,7 @@ module "jenkins-vm" {
   }
 }
 
-module "webserver-vm" {
+module "webserver_vm" {
   source                        = "Azure/compute/azurerm"
   resource_group_name           = azurerm_resource_group.devops.name
   location                      = azurerm_resource_group.devops.location
@@ -48,81 +48,79 @@ module "webserver-vm" {
 module "vnet" {
   source = "Azure/vnet/azurerm"
   resource_group_name = azurerm_resource_group.devops.name
-  vnet_name           = "devops-vnet"
+  vnet_name           = "devops_vnet"
   address_space       = ["192.168.0.0/16"]
-  subnet_prefixes     = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24"]
-  subnet_names        = ["subnet-a", "subnet-b", "subnet-c"]
+  subnet_prefixes     = ["192.168.0.0/24", "192.168.1.0/24"]
+  subnet_names        = ["subnet_a", "subnet_b"]
 
-  nsg_ids = {
-    subnet-a = module.jenkins-nsg.network_security_group_id
-    subnet-b = module.webserver-nsg.network_security_group_id
-  }
-
-  tags = {
-    environment = "dev"
-  }
-}
-
-module "jenkins-nsg" {
-  source                = "Azure/network-security-group/azurerm"
-  version               = "3.0.0"
-  resource_group_name   = azurerm_resource_group.devops.name
-  security_group_name   = "jenkins-nsg"
-  predefined_rules = [
-    {
-      name     = "SSH"
-      priority = "500"
-    }
-  ]
-  custom_rules = [
-    {
-      name                   = "Jenkins_8080"
-      priority               = "510"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "8080"
-      description            = "Jenkins_8080"
-    }
-  ]
+  # nsg_ids = {
+  #   subnet_a = module.jenkins_nsg.network_security_group_id
+  #   subnet_b = module.webserver_nsg.network_security_group_id
+  # }
 
   tags = {
     environment = "dev"
   }
 }
 
-module "webserver-nsg" {
-  source                = "Azure/network-security-group/azurerm"
-  version               = "3.0.0"
-  resource_group_name   = azurerm_resource_group.devops.name
-  security_group_name   = "webserver-nsg"
-  predefined_rules = [
-    {
-      name     = "SSH"
-      priority = "500"
-    },
-    {
-      name     = "HTTP"
-      priority = "510"
-    },
-    {
-      name     = "HTTPS"
-      priority = "520"
-    }
-  ]
-  custom_rules = [
-    {
-      name                   = "phpmyadmin_3333"
-      priority               = "530"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "3333"
-      description            = "phpmyadmin_3333"
-    }
-  ]
+# module "jenkins_nsg" {
+#   source                = "Azure/network-security-group/azurerm"
+#   resource_group_name   = azurerm_resource_group.devops.name
+#   security_group_name   = "jenkins_nsg"
+#   predefined_rules = [
+#     {
+#       name     = "SSH"
+#       priority = "500"
+#     }
+#   ]
+#   custom_rules = [
+#     {
+#       name                   = "Jenkins_8080"
+#       priority               = "510"
+#       direction              = "Inbound"
+#       access                 = "Allow"
+#       protocol               = "tcp"
+#       destination_port_range = "8080"
+#       description            = "Jenkins_8080"
+#     }
+#   ]
 
-  tags = {
-    environment = "dev"
-  }
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# module "webserver_nsg" {
+#   source                = "Azure/network-security-group/azurerm"
+#   resource_group_name   = azurerm_resource_group.devops.name
+#   security_group_name   = "webserver_nsg"
+#   predefined_rules = [
+#     {
+#       name     = "SSH"
+#       priority = "500"
+#     },
+#     {
+#       name     = "HTTP"
+#       priority = "510"
+#     },
+#     {
+#       name     = "HTTPS"
+#       priority = "520"
+#     }
+#   ]
+#   custom_rules = [
+#     {
+#       name                   = "phpmyadmin_3333"
+#       priority               = "530"
+#       direction              = "Inbound"
+#       access                 = "Allow"
+#       protocol               = "tcp"
+#       destination_port_range = "3333"
+#       description            = "phpmyadmin_3333"
+#     }
+#   ]
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,32 @@
-# output "jenkins_vm_public_ips" {
-#   value = module.jenkins-vm.public_ip_address
-# } 
+output "jenkins_vm_public_ips" {
+  value = module.jenkins_vm.public_ip_address
+} 
 
 # # output "jenkins_vm_private_ips" {
-# #   value = module.jenkins-vm.network_interface_private_ip
+# #   value = module.jenkins_vm.network_interface_private_ip
 # # }
 
-# output "jenkins_vm_url" {
-#   value = "http://${element(module.jenkins-vm.public_ip_address, 0)}:8080"
-# }
+output "jenkins_vm_url" {
+  value = "http://${element(module.jenkins_vm.public_ip_address, 0)}:8080"
+}
 
-# output "webserver-vm_public_ips" {
-#   value = module.webserver-vm.public_ip_address
-# } 
+output "webserver-vm_public_ips" {
+  value = module.webserver_vm.public_ip_address
+} 
 
 # # output "webserver-vm_private_ips" {
-# #   value = module.webserver-vm.network_interface_private_ip
+# #   value = module.webserver_vm.network_interface_private_ip
 # # }
 
-# output "webserver-vm_url" {
-#   value = "http://${element(module.webserver-vm.public_ip_address, 0)}"
-# }
+output "webserver-vm_url" {
+  value = "http://${element(module.webserver_vm.public_ip_address, 0)}"
+}
 
 ### The Ansible inventory file
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl", {
-    jenkins-vm-ip = module.jenkins-vm.public_ip_address
-    webserver-vm-ip = module.webserver-vm.public_ip_address
+    jenkins-vm-ip = module.jenkins_vm.public_ip_address
+    webserver-vm-ip = module.webserver_vm.public_ip_address
   })
   filename = "../ansible-demo/hosts.txt"
 }
