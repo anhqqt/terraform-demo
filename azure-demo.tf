ad928d24f096d9416333d6260c5556236ff9f5d7
Anh Quach <sheid1309@gmail.com>
2020-08-19 00:09:46 +0700

    Update main.tf using azure backend

--- azure-demo.tf
+++ azure-demo.tf
@@ -5,12 +5,13 @@ resource "azurerm_resource_group" "devops" {
 
 module "jenkins_vm" {
   source                        = "Azure/compute/azurerm"
+  # version                       = "3.4.0"
   resource_group_name           = azurerm_resource_group.devops.name
   location                      = azurerm_resource_group.devops.location
   vm_hostname                   = "jenkins"
   admin_username                = "matdecha"
   ssh_key                       = "${var.public_key}"
-  # remote_port                   = "${var.vm_remote_port["SSH"]}"
+  remote_port                   = "${var.vm_remote_port["SSH"]}"
   vm_os_publisher               = "OpenLogic"
   vm_os_offer                   = "CentOS"
   vm_os_sku                     = "7.7"
@@ -26,12 +27,13 @@ module "jenkins_vm" {
 
 module "webserver_vm" {
   source                        = "Azure/compute/azurerm"
+  # version                       = "3.4.0"
   resource_group_name           = azurerm_resource_group.devops.name
   location                      = azurerm_resource_group.devops.location
   vm_hostname                   = "webserver"
   admin_username                = "matdecha"
   ssh_key                       = "${var.public_key}"
-  # remote_port                   = "${var.vm_remote_port["SSH"]}"
+  remote_port                   = "${var.vm_remote_port["SSH"]}"
   vm_os_publisher               = "OpenLogic"
   vm_os_offer                   = "CentOS"
   vm_os_sku                     = "7.7"
@@ -53,74 +55,74 @@ module "vnet" {
   subnet_prefixes     = ["192.168.0.0/24", "192.168.1.0/24"]
   subnet_names        = ["subnet_a", "subnet_b"]
 
-  nsg_ids = {
-    subnet_a = module.jenkins_nsg.network_security_group_id
-    subnet_b = module.webserver_nsg.network_security_group_id
-  }
+  # nsg_ids = {
+  #   subnet_a = module.jenkins_nsg.network_security_group_id
+  #   subnet_b = module.webserver_nsg.network_security_group_id
+  # }
 
   tags = {
     environment = "dev"
   }
 }
 
-module "jenkins_nsg" {
-  source                = "Azure/network-security-group/azurerm"
-  resource_group_name   = azurerm_resource_group.devops.name
-  security_group_name   = "jenkins_nsg"
-  predefined_rules = [
-    {
-      name     = "SSH"
-      priority = "500"
-    }
-  ]
-  custom_rules = [
-    {
-      name                   = "Jenkins_8080"
-      priority               = "510"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "8080"
-      description            = "Jenkins_8080"
-    }
-  ]
+# module "jenkins_nsg" {
+#   source                = "Azure/network-security-group/azurerm"
+#   resource_group_name   = azurerm_resource_group.devops.name
+#   security_group_name   = "jenkins_nsg"
+#   predefined_rules = [
+#     {
+#       name     = "SSH"
+#       priority = "500"
+#     }
+#   ]
+#   custom_rules = [
+#     {
+#       name                   = "Jenkins_8080"
+#       priority               = "510"
+#       direction              = "Inbound"
+#       access                 = "Allow"
+#       protocol               = "tcp"
+#       destination_port_range = "8080"
+#       description            = "Jenkins_8080"
+#     }
+#   ]
 
-  tags = {
-    environment = "dev"
-  }
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
 
-module "webserver_nsg" {
-  source                = "Azure/network-security-group/azurerm"
-  resource_group_name   = azurerm_resource_group.devops.name
-  security_group_name   = "webserver_nsg"
-  predefined_rules = [
-    {
-      name     = "SSH"
-      priority = "500"
-    },
-    {
-      name     = "HTTP"
-      priority = "510"
-    },
-    {
-      name     = "HTTPS"
-      priority = "520"
-    }
-  ]
-  custom_rules = [
-    {
-      name                   = "phpmyadmin_3333"
-      priority               = "530"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "3333"
-      description            = "phpmyadmin_3333"
-    }
-  ]
+# module "webserver_nsg" {
+#   source                = "Azure/network-security-group/azurerm"
+#   resource_group_name   = azurerm_resource_group.devops.name
+#   security_group_name   = "webserver_nsg"
+#   predefined_rules = [
+#     {
+#       name     = "SSH"
+#       priority = "500"
+#     },
+#     {
+#       name     = "HTTP"
+#       priority = "510"
+#     },
+#     {
+#       name     = "HTTPS"
+#       priority = "520"
+#     }
+#   ]
+#   custom_rules = [
+#     {
+#       name                   = "phpmyadmin_3333"
+#       priority               = "530"
+#       direction              = "Inbound"
+#       access                 = "Allow"
+#       protocol               = "tcp"
+#       destination_port_range = "3333"
+#       description            = "phpmyadmin_3333"
+#     }
+#   ]
 
-  tags = {
-    environment = "dev"
-  }
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 provider "azurerm" {
+  version = "2.21.0"
   subscription_id = "d0767e87-e2a6-4477-a762-768ce1367057"
   features {}
+}
+
+# Backend configuration
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate"
+    storage_account_name  = "tfstate4445"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
 }
